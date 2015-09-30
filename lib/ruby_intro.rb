5f7a0e08086d70d037e5d700d5c1855a3583f3f7
lizhealy <lizhealy8@gmail.com>
2015-09-30 23:25:44 +0000

    Updated

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,80 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  else
+    sum = 0
+    arr.each do |i|
+      sum += i
+    end
+    return sum
+  end
 end
 
+
 def max_2_sum arr
-  # YOUR CODE HERE
+    if arr.length == 0 
+        return 0
+    elsif arr.length == 1
+        return arr[0]
+    else 
+      arr.sort!
+      return arr[-1] + arr[-2]
+    end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  (0..arr.length-1).any? do |i| 
+    arr[i+1..-1].any? { |j| j + arr[i] == n }
+  end
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.downcase
+  /^[bcdfghjklmnpqrstvwxyz]/i === s
 end
 
+
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless /^[01]+$/.match(s) != nil
+  return s.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    
+    raise ArgumentError if isbn.length == 0 || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def isbn= number
+    @isbn = number
+  end
+  
+  def price
+    @price
+  end
+  
+  def price= value
+    @price = value
+  end
+  
+  
+  def price_as_string
+    return "$#{ '%.02f' % @price }"
+  end
 end
